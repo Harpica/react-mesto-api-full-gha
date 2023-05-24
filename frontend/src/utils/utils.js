3192328f9a2ab3a1888c0dbc9024c71bc2cf7830
Harpica <harpy.ka@gmail.com>
2023-05-24 14:03:07 +0400

    fix: deleted unused

--- frontend/src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-function clearCookies() {
-  const cookies = document.cookie.split(';');
-  cookies.forEach((cookie) => {
-    document.cookie = cookie + '=; expires=' + new Date(0).toUTCString();
-  });
-}
-
-export { clearCookies };
